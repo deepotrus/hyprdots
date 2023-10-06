5152f71a9c8936628e1e1acf644771ec08edc57a
Andrei Potra <88773087+deepotrus@users.noreply.github.com>
2023-10-06 16:58:21 +0000

    Delete toggle-bluetooth.sh

--- toggle-bluetooth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check bluetooth status
-if bluetoothctl show | grep -q "Powered: yes"; then
-  bluetoothctl power off
-else
-  # bluetooth is off, so turn it on
-  bluetoothctl power on
-fi
