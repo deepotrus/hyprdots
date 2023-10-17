8cc674c8c9fbc298ef8536bce76898e51855adc4
Andrei Potra <88773087+deepotrus@users.noreply.github.com>
2023-10-17 21:18:45 +0000

    Keep terminal opened after running script

--- waybar/weather/start.sh
+++ waybar/weather/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # -e runs the script in the new opened terminal
-kitty -e /bin/bash -c '~/.config/waybar/weather/ask-weather.sh; exec /bin/bash '
+kitty -e /bin/bash -c '~/.config/waybar/weather/ask-weather.sh; read -p "Press any key to exit > " -n1 junk'
+
